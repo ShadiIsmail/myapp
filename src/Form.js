8c441e2af4c32f97db5b984e809599abe9555c22
Shadi <shadi28eg@gmail.com>
2018-12-14 11:45:29 -0500

    second

--- src/Form.js
+++ src/Form.js
@@ -1,37 +1,5 @@
 import React from 'react'
 
-function CheckBalance(props) {
-
-let isParenthesisMatching = (str) => {
-    let stack = [];
-
-    let open = {
-        '{': '}',
-        '[': ']',
-        '(': ')'
-    };
-
-    let closed = {
-        '}': true,
-        ']': true,
-        ')': true
-    }
-
-    for (let i = 0; i < str.length; i++) {
-
-        let char = str[i];
-
-        if (open[char]) {
-            stack.push(char);
-        } else if (closed[char]) {
-            if (open[stack.pop()] !== char) return false;
-        }
-    }
-    return stack.length === 0;
-}
-}
-
-
 class Form extends React.Component {
   constructor (props) {
       super(props);
@@ -52,20 +20,47 @@ class Form extends React.Component {
   handleSubmit (event) {
       console.log('Form value: ' + this.state.inputvalue);
       event.preventDefault();
+      let isParenthesisMatching = (str) => {
+          let stack = [];
+
+          let open = {
+              '{': '}',
+              '[': ']',
+              '(': ')'
+          };
+
+          let closed = {
+              '}': true,
+              ']': true,
+              ')': true
+          }
+
+          for (let i = 0; i < str.length; i++) {
+
+              let char = str[i];
+
+              if (open[char]) {
+                  stack.push(char);
+              } else if (closed[char]) {
+                  if (open[stack.pop()] !== char) return false;
+              }
+          }
+          return stack.length === 0;
+      }
   }
 
 
   render() {
     return (
       <div className="App">
-      
+
           <form onSubmit={this.handleSubmit}>
               <textarea className="textarea" placeholder="Your Code Goes Here Bro!" value={this.state.inputvalue} onChange={this.handleChange}/> <br/>
               <button className="btn btn-default"
        onClick={this.props.handleClick}>{this.props.label} Click Me If you Dare!</button>
           </form>
       </div>
-    
+
     );
   }
 }
